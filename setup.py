3942ab3438eaaeaecd0e44962a534d6b23f5e3b1
kosyak <vadim@kosov.eu>
2021-08-24 18:21:00 +0000

    Added py-to-pickle

--- setup.py
+++ setup.py
@@ -1,9 +1,39 @@
 #!/usr/bin/env python
 
-import versioneer
-from os.path import exists
-from setuptools import setup
+import os
+import sys
+from glob import glob
 
+from setuptools import Extension, setup
+
+import versioneer
+
+if glob("*.pyx"):
+    from Cython.Build import cythonize
+    module_file_ext = 'pyx'
+else:
+    module_file_ext = 'cpp'
+
+extensions = []
+extra_compile_args = ['-DLIB']
+if not sys.platform.startswith('win'):
+    extra_compile_args.append('-std=c++11')
+extra_link_args = []  # -shared -fPIC
+include_dirs = []
+
+extensions.append(Extension(
+    "pytopickle",
+    glob("pandahouse/*.cpp"),
+    include_dirs=[],
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
+    language="c++",
+))
+
+if module_file_ext == 'pyx':
+    ext_modules = cythonize(extensions, include_path=include_dirs, compiler_directives={'embedsignature': True})
+else:
+    ext_modules = extensions
 
 setup(name='pandahouse',
       version=versioneer.get_version(),
@@ -18,6 +48,6 @@ setup(name='pandahouse',
       tests_require=['pytest'],
       setup_requires=['pytest-runner'],
       install_requires=['pandas', 'requests', 'toolz'],
-      long_description=(open('README.rst').read() if exists('README.rst')
-                        else ''),
-      zip_safe=False)
+      long_description=(open('README.rst').read() if os.path.exists('README.rst') else ''),
+      zip_safe=False,
+      ext_modules=ext_modules)
