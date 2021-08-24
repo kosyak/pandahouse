c8ad86a20cf0e5cb15faed0acf324dee310999d8
kosyak <vadim@kosov.eu>
2021-08-24 18:55:26 +0000

    Using py-to-pickle

--- pandahouse/utils.py
+++ pandahouse/utils.py
@@ -1,8 +1,9 @@
+import ctypes
 import re
 import json
 import codecs
 import ast
-
+from typing import Union
 
 SPECIAL_CHARS = {
     '\b': '\\b',
@@ -47,4 +48,24 @@ def decode_escapes(s):
 
 
 def decode_array(clickhouse_array):
-    return ast.literal_eval(clickhouse_array)
+    return py_to_pickle(clickhouse_array)
+    # return ast.literal_eval(clickhouse_array)
+
+
+def py_to_pickle(s: Union[str, bytes]) -> bytes:
+    if isinstance(s, bytes):
+        in_bytes = s
+    else:
+        in_bytes = s.encode("utf8")
+    in_ = ctypes.create_string_buffer(in_bytes)
+    in_len = len(in_bytes)
+    out_len = in_len + 1000  # should always be enough (some buffer len + len of literal Python code)
+    out_ = ctypes.create_string_buffer(out_len)
+
+    lib = ctypes.CDLL("pytopickle")
+    lib.py_to_pickle.argtypes = (ctypes.c_char_p, ctypes.c_size_t, ctypes.c_char_p, ctypes.c_size_t)
+    lib.py_to_pickle.restype = ctypes.c_int
+
+    res = lib.py_to_pickle(in_, in_len, out_, out_len)
+    assert res == 0, "there was some error in %r" % in_bytes
+    return out_.raw
