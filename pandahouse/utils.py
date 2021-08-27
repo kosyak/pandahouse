03d2c94a38d4a72e8780086dd4db72d8822a0e32
kosyak <vadim@kosov.eu>
2021-08-27 14:47:15 +0300

    C debug

--- pandahouse/utils.py
+++ pandahouse/utils.py
@@ -61,4 +61,6 @@ def py_to_pickle(s: Union[str, bytes]) -> bytes:
         in_bytes = s.encode("utf8")
     in_len = len(in_bytes)
 
-    return lib(in_bytes, in_len).encode("utf8")
+    lib_result = lib(in_bytes, in_len)
+    return lib_result.encode("utf8")
+
